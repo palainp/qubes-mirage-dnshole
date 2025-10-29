9f1d1b7244f331c3846df00e09f0517c6d52e1da
Pierre Alain <65669679+palainp@users.noreply.github.com>
2025-10-29 11:59:33 +0100

    add GH actions (#1)
    * add GH actions
    
    * add ocamlformat version for GH actions
    
    * formatted code
    
    * update hashsum after ocamlformat
    
    * update names in GH actions
    
    * update the hashsum file
    
    * add interface files
    
    ---------
    
    Co-authored-by: Automated ocamlformat GitHub action, developed by robur.coop <autoformat@robur.coop>

---
+++ build-with.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -eu
+
+if [[ $# -ne 1 ]] ; then
+	echo "Usage: build-with.sh { docker | podman }"
+	exit 1
+fi
+
+builder=$1
+case $builder in
+	docker|podman)
+	;;
+	*)
+	echo "You should use either docker or podman for building"
+	exit 2
+esac
+
+echo Building $builder image with dependencies..
+$builder build -t qubes-mirage-dnshole .
+echo Building Dnshole...
+$builder run --rm -i -v `pwd`:/tmp/orb-build:Z qubes-mirage-dnshole
+echo "SHA2 of build:     $(sha256sum ./dist/qubes-dnshole.xen | cut -d' ' -f1)"
+echo "SHA2 current head: $(cat qubes-dnshole.sha256 | cut -d' ' -f1)"
+#echo "SHA2 last release: $(cat qubes-dnshole-release.sha256 | cut -d' ' -f1)"
+echo "(hashes should match for head versions)"
---
+++ vif.mli
@@ -0,0 +1,24 @@
+module Netbackend : module type of Backend.Make (Xenstore.Make (Xen_os.Xs))
+module Client_ethernet : module type of Ethernet.Make (Netbackend)
+module Client_arp : Arp.S
+
+module Client_ip :
+    module type of Static_ipv4.Make (Client_ethernet) (Client_arp)
+
+type t = {
+  ipaddr : Ipaddr.V4.t * Ipaddr.V4.t;
+  mac : Macaddr.t * Macaddr.t;
+  ethernet : Client_ethernet.t;
+  arp : Client_arp.t;
+  ip : Client_ip.t;
+  domid : int;
+}
+
+val make :
+  Netbackend.t ->
+  Dao.Client_vif.t ->
+  gateway:Ipaddr.V4.t ->
+  Ipaddr.V4.t ->
+  t Lwt.t
+
+val pp : t Fmt.t
